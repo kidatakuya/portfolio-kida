65918397a04304650af4c90d4d1b84334e2e6aeb
kidatakuya <kida.takuya.1234@gmail.com>
2020-11-04 17:16:02 +0900

    ecoloop詳細ひとまず

--- portfolio/src/components/worksDetails/ecoloop.vue
+++ portfolio/src/components/worksDetails/ecoloop.vue
@@ -35,41 +35,69 @@
                 </div>
             </div>
             <section class="ecoloop__content__backWarp__planning">
-                <section class="ecoloop__content__backWarp__planning__category problembox">
-                    <h3><span>現状問題</span></h3>
+                <section class="ecoloop__content__backWarp__planning__category overviewbox">
+                    <h3><span>概要</span></h3>
                     <p>
                         今、世界中でゴミの量の多さが問題視されている。それは日本も例外ではありません。日本でも生ゴミの排出量が大きな問題になっている。そのため、日本では生ゴミによるバイオマス、バイオガス発電のプロジェクトに取り組む市や街が存在する。<br>
                         ECO LOOPでは、上記のプロジェクトに取り組む市や街をサポートする、街が一体となってエコ活動をしているという意識を持てるようなにするサービスです。<br>
                         このプロジェクトはSDGs（2016年から2030年までの国際目標）の7（エネルギーをみんなにそしてクリーンに）と、11（住み続けられるまちづくりを）に該当している。
                     </p>
-                    <div>
-                        <p></p>
-                        <div></div>
+                    <p>SDGs -外務省  Ministry of Foreign Affairs of Japan</p>
+                    <div class="ecoloop__content__backWarp__planning__category__reference">
+                        <a href="https://www.mofa.go.jp/mofaj/gaiko/oda/sdgs/about/index.html" target="_blank">https://www.mofa.go.jp/mofaj/gaiko/oda/sdgs/about/index.html</a>
+                        <div class="Sdgs"></div>
+                        <div class="Sdgs"></div>
                     </div>
+                </section>
+                <section class="ecoloop__content__backWarp__planning__category problembox">
+                    <h3><span>現状問題</span></h3>
+                   <p class="problembox__text">
+                       日本ではゴミの排出量が問題になっており、日本のゴミの排出量は<span>東京ドーム１１５杯分（4289キロ）</span>にまで及んでいる。<span>ゴミの処理費用はおよそ２兆円</span>。その半分が税金でで払われている。地球温暖化により環境破壊が進んでいる。
+                   </p>
+                </section>
+                 <section class="ecoloop__content__backWarp__planning__category cause">
+                    <h3><span>原因</span></h3>
+                    <p class="cause__text">
+                        ・食べ残し<br>
+                        ・賞味期限ぎれ
+                    </p>
                 </section>
                 <section class="ecoloop__content__backWarp__planning__category solutionbox">
                     <h3><span>解決案</span></h3>
-                    <p>sdasdasdasdasd</p>
+                    <p class="solutionbox__text">
+                        多かれ少なかれ廃棄出てしまうから再利用してしまおう
+                    </p>
                 </section>
                 <section class="ecoloop__content__backWarp__planning__category purposebox">
                     <h3><span>目的</span></h3>
-                    <p>asdasdasd</p>
-                </section>
-                <section class="ecoloop__content__backWarp__planning__category overviewbox">
-                    <h3><span>概要</span></h3>
-                    <p>asdasdasd</p>
+                    <p class="purposebox__text">
+                        ゴミの処理量を減らし、ゴミを再利用する
+                    </p>
                 </section>
                 <section class="ecoloop__content__backWarp__planning__category meritbox">
                     <h3><span>メリット</span></h3>
-                    <p>asdasda</p>
+                    <p class="meritbox__text">
+                        ・ゴミの処分料が減り、ゴミ処理に必要な費用を削減できる。<br>
+                        ・再利用することで電気に作り替えられる。
+                    </p>
                 </section>
-                <section class="ecoloop__content__backWarp__planning__category profitbox">
+                <section class="ecoloop__content__backWarp__planning__category modelbox">
                     <h3><span>利益</span></h3>
-                    <p>asdasdasdas</p>
+                    <p class="model__text">
+                        <span>横浜市 生ごみバイオガス化の検討 ２０１９年８月２８日</span><br>
+                        <span>生ゴミのバイオガスかを検討している横浜市にこのサービスをモデルケースとして使ってもらう。</span><br>
+                        <span><a href="https://www.city.yokohama.lg.jp/kurashi/sumai-kurashi/gomi-recycle/namagomi/namagas.html" target="_blank">https://www.city.yokohama.lg.jp/kurashi/sumai-kurashi/gomi-recycle/namagomi/namagas.html</a></span>
+
+                    </p>
                 </section>
                 <section class="ecoloop__content__backWarp__planning__category leadbox">
                     <h3><span>導線</span></h3>
-                    <p>asasdasdasdasd</p>
+                    <p class="leadbox__text">
+                        ・SNS<br>
+                        ・ホームページでの告知<br>
+                        ・チラシやポスターで告知
+                    </p>
+                    <div class="leadbox__advertising"></div>
                 </section>
             </section>
         </section>
@@ -102,7 +130,20 @@ export default {
   components: {
    DetailsHeader,
    Footer
-  }
+  },
+   mounted(){
+       const leadImg = document.querySelector(".leadbox__advertising")
+
+        function imgwin(){
+            window.open("ecoloop-lead.jpg", "imgwindow", "width=866,height=780");
+        }
+
+        leadImg.onclick=()=>{
+            imgwin();
+        }
+
+   }
+
 }
 </script>
 
@@ -187,14 +228,88 @@ export default {
                 &__category{
                     margin-bottom: 56px;
                      h3{
-                    text-align: center;
-                    font-size: 24px;
-                    color: #4D4D4D;
+                        text-align: center;
+                        font-size: 24px;
+                        color: #4D4D4D;
+                        margin-bottom: 40px;
+                    }
+                    p{
+                        font-size: 16px;
+                        color: #A0A0A0;
+                    }
+                &__reference{
+                    margin: 40px 0;
+                    display: flex;
+                    align-items: center;
+                    a{
+                        font-size: 16px;
+                        color: #4D4D4D;
+                    }
+                    .Sdgs{
+                        width: 60px;
+                        height: 60px;
+                        background-position: center;
+                        background-size: contain;
+                        margin-left: 40px;
+                        &:nth-of-type(1){  
+                            background-image: url(../../assets/img/SDGs7.svg);
+                        }
+                        &:nth-of-type(2){  
+                            background-image: url(../../assets/img/SDGs11.svg);
+                            margin-left: 20px;
+                        }
+                    }
                 }
-                p{
-                    font-size: 16px;
-                    color: #A0A0A0;
-                }
+                    .problembox{
+                            &__text{
+                                span{
+                                    color: #4D4D4D;
+                                }
+                            }
+                        }
+                    .cause{
+                        &__text{
+                            text-align: center;
+                            font-size: 20px;
+                            color: #4D4D4D;
+                        }
+                    }
+                    .solutionbox{
+                        &__text{
+                             @extend .cause__text;
+                            
+                        }
+                    }
+                    .purposebox{
+                        &__text{
+                             @extend .cause__text;
+                        }
+                    }
+                    .meritbox{
+                        &__text{
+                            @extend .cause__text;
+                        }
+                    }
+                    .model{
+                        &__text{
+                             @extend .cause__text;
+                        }
+                    }
+                    .leadbox{
+                        &__text{
+                            @extend .cause__text;
+                        }
+                        &__advertising{
+                            width: 560px;
+                            height: 400px;
+                            background-image: url(../../assets/img/ecoloop-lead.jpg);
+                            background-size: contain;
+                            background-position: center;
+                            cursor: pointer;
+                            margin: 0 auto;
+                        }
+                    }
+                
                 }
             }
           }
